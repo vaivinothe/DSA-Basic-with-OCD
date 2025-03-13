24dcb9e0423c9696d590dc5fe55c6c4fb8d69b1e
VINOD Vaishnav <vinodvaishnav26012007@gmail.com>
2025-03-13 19:20:33 +0530

    Update DAY11_PROBLEMS.cpp

--- DAY11_PROBLEMS.cpp
+++ DAY11_PROBLEMS.cpp
@@ -22,5 +22,53 @@ public:
         return nums;
     }
 };
+// TC : O(n^2)
+// SC : O(n^2)
 Q-2: https://leetcode.com/problems/rotate-image/description/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        #include<iostream>
+        #include<vector>
+        #include<algorithm>
+        using namespace std;
+            int n = matrix.size();
+            // cout << "enter :";
+            // cin >> n;
+            // int count = 0;
+            // vector<vector<int>>matrix(n , vector<int>(n));
+            // for (int i = 0; i < n; i++)
+            // { for (int j = 0; j < n ; j++)
+            // {
+            //     cin >> matrix[i][j];
+            // }
+            // }
+            for (int i = 0; i < n; i++)
+            {
+                for  (int j = 0; j <=i ; j++)
+                {       int temp;
+                        temp = matrix[i][j];
+                        matrix[i][j] = matrix[j][i];
+                        matrix[j][i]= temp;
+                        // count++;
+                }
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n/2 ; j++)
+                {   int temp;
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][n-j-1];
+                    matrix[i][n-j-1]= temp;
+                }
+                
+            }
+        // return {matrix}; 
+    }
+};
+// TC : O(n^2)
+// SC : O(1)
+
+Q-3: https://leetcode.com/problems/spiral-matrix/description/ 
 
