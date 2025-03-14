80c0025f030140b24fe18e0d2ff27932695c2f0b
VINOD Vaishnav <vinodvaishnav26012007@gmail.com>
2025-03-14 10:15:07 +0530

    Update DAY11_PROBLEMS.cpp

--- DAY11_PROBLEMS.cpp
+++ DAY11_PROBLEMS.cpp
@@ -24,6 +24,7 @@ public:
 };
 // TC : O(n^2)
 // SC : O(n^2)
+
 Q-2: https://leetcode.com/problems/rotate-image/description/
 class Solution {
 public:
@@ -46,13 +47,11 @@ public:
             for (int i = 0; i < n; i++)
             {
                 for  (int j = 0; j <=i ; j++)
-                {       int temp;
-                        temp = matrix[i][j];
-                        matrix[i][j] = matrix[j][i];
-                        matrix[j][i]= temp;
-                        // count++;
-                }
-                
+                {   int temp;
+                    temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i]= temp;       
+                }   
             }
             for (int i = 0; i < n; i++)
             {
@@ -61,14 +60,66 @@ public:
                     temp = matrix[i][j];
                     matrix[i][j] = matrix[i][n-j-1];
                     matrix[i][n-j-1]= temp;
-                }
-                
-            }
-        // return {matrix}; 
+                }   
+            } 
     }
 };
 // TC : O(n^2)
 // SC : O(1)
 
 Q-3: https://leetcode.com/problems/spiral-matrix/description/ 
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        #include<iostream>
+        #include<vector>
+        #include<algorithm>
+        using namespace std;
+            int n = matrix.size();
+            int m = matrix[0].size();
+            int min_row = 0;
+            int max_row = n-1;
+            int min_col = 0;
+            int max_col =m-1;
+            int count = 0;
+            vector<int>result(n*m);
+            // vector<vector<int>>matrix(n , vector<int>(n));
+            // for (int i = 0; i < n; i++)
+            // { for (int j = 0; j < n ; j++)
+            // {
+            //     cin >> matrix[i][j];
+            // }
+            // }
+        while (count<n*m)
+        {
+            for (int  i = min_col; i <= max_col && count<n*m; i++)
+            {
+                result[count] = matrix[min_row][i];
+                count++;
+            }
+            min_row++;
+            for (int  i = min_row; i <= max_row && count<n*m; i++)
+            {
+                result[count] = matrix[i][max_col];
+                count++;
+            }
+            max_col--;
+            for (int  i = max_col; i >= min_col  && count<n*m; i--)
+            {
+                result[count] = matrix[max_row][i];
+                count++;
+            }
+            max_row--;
+             for (int  i = max_row; i >= min_row && count<n*m; i--)
+            {
+                result[count] = matrix[i][min_col];
+                count++;
+            }
+            min_col++;
+        }
+    return {result};   
+    }
+};
+// TC :O(n*m)
+// SC :O(n*m)
 
